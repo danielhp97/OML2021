fe4ba5e8d97f96de2b4f5c8dc6ca1db8e3a329f7
danielhp97 <danielhp1234@gmail.com>
2021-02-24 23:03:08 +0000

    corrected norm

--- tpc/tpc1/ex3.py
+++ tpc/tpc1/ex3.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from numpy import linalg as LA
 ## Pre Requisitos
 
 # Upload Dataset
@@ -11,9 +12,19 @@ data
 # Divide Dataset
 
 x_train, x_test = train_test_split(data.values.tolist(), test_size=0.20)
-
 x_train
 x_test
+# Dividindo dados
+def Extract(lst,i):
+    return [item[i] for item in lst]
+
+X_data_train = Extract(x_train,0)
+X_data_train
+X_data_test = Extract(x_test,0)
+X_data_test
+
+Y_data_train = Extract(x_train,1)
+Y_data_test = Extract(x_test,1)
 
 
 # Exercicio 3
@@ -22,7 +33,7 @@ def backtrackingArmijo():
     eta_k=1
     while( fun_Custo(w+eta_k*s_k,X,Y)> Custo_k+delta*eta_k*grad_full*s_k):
         eta_k=eta_k/2
-        if eta_k*norm(s_k)<=1e-8:
+        if eta_k*LA.norm(s_k)<=1e-8:
             eta_k=1
     return eta_k
 
@@ -32,6 +43,7 @@ def passo(w,Custo_k,grad_full,s_k,X,Y,k,nt):
     return eta_k
 
 def fun_Custo(w,X,Y):
+    val = 0
     I= len(w)-1
     nt=len(X)
     for i in range(1,nt):
@@ -40,12 +52,14 @@ def fun_Custo(w,X,Y):
     return(val)
 
 def gradiente(w,x,I):
+    sum = 0
     for i in range(0,I):
         sum += np.dot(w,pow(x,i)) # must have 2 np arrays.
     return sum
 
 
-def gradiente_batch(w,I,x,y):
+def gradiente_batch(w,x,y):
+    sum_gradiente = 0
     n = len(x) - 1 if x else None;
     if n == None : raise Exception("Empty Array")
     for i in range(1,n):
@@ -55,49 +69,30 @@ def gradiente_batch(w,I,x,y):
     return sum_gradiente
 
 
-def stop_condition():
-    print("Calcular stop condition")
 
 # escolher grau de polinomio a ajustar.
 I=7
-
 # Método de gradiente completo (gradiente de batch)
 # Vamos utilizar algoritmo de Armijo, para a procura
-# ou seja, memso método de G=1 e P=1 (segundo o exemplo da prof.)
-
-
-# definimos ponto inicial como w^(1) = (0,0,0,...0)
-w1 = np.full((1,I),0);
-
-# temos que definir um valor de tolerancia
-tol = 1e-4
-# definimos também o nr max de iterações
+# ou seja, mesmo método de G=1 e P=1 (segundo o exemplo da prof.)
+w1 = np.full((1,I),0); # definimos ponto inicial como w^(1) = (0,0,0,...0)
+tol = 1e-4 # temos que definir um valor de tolerancia
 nt = len (x_train) #onde nt é o nr de obs do treino
-maxit = 10*nt
+maxit = 10*nt # definimos também o nr max de iterações
 k = 1 # ponto inicial
+
 def principal():
-    while(norm(gradiente_batch() > tol && k< max it)): # criterios de paragem
-        # calcular gradente no ponto wk
-        grad_k = gradiente_batch(wk,x,y)
-        # calcular direcção de procura
-        s_k = -grad_k
-        # calcular comprimento do passo
-
-        # calcular Função objectivo no ponto wk
-        Custo_k = fun_Custo(w,X,Y)
-        # Calcular o gradiente completo no ponto wk
-        grad_full = gradiente_batch(w,X,Y)
-        # Calcular comprimento do passo
-        eta_k = passo(w,Custo_k,grad_full,s_k,X,Y,k,nt)
-        # Calcular novo ponto
-        wk = wk + eta_k*sk
-        k = k+1
-        # Inserir algoritmo para calcular ponto necessários a calcular gráfico
-
+    while(LA.norm(gradiente_batch(w1,X_data_train,Y_data_train) > tol) and k< maxit): # criterios de paragem
+        grad_k = gradiente_batch(w1,X_data_train,Y_data_train) # calcular gradente no ponto wk
+        s_k = -grad_k # calcular direcção de procura
+        Custo_k = fun_Custo(w1,X_data_train,Y_data_train) # calcular Função objectivo no ponto wk
+        grad_full = gradiente_batch(w1,X_data_train,Y_dat_train) # Calcular o gradiente completo no ponto wk
+        eta_k = passo(w1,Custo_k,grad_full,s_k,X_data_train,Y_dat_train,k,nt) # Calcular comprimento do passo
+        wk = wk + eta_k*sk # Calcular novo ponto
+        k = k+1 # Inserir algoritmo para calcular ponto necessários a calcular gráfico
     print("A solução ótima")
-
     print("In-Sample Error")
-
     print("Out-Sample Error")
 
+principal()
     #Print do grafico com valor da Função CUsto ao longo do processo Iterativo
